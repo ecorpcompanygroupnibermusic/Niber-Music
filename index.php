bcb3daf1a0504e28268c81fede53f68dea75ab3c
ecorpcompanygroupnibermusic <126945950+ecorpcompanygroupnibermusic@users.noreply.github.com>
2023-03-11 13:47:48 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>send Data to Databse</title>
+</head>
+<body>
+    <form action="#" method="post">
+    <input type="text" name="name" required>
+    <br><br>
+    <input type="email" name="email" required>
+    <br><br>
+    <input type="password" name="password" required>
+    <br><br>
+    <button>Sumbit</button>
+    </form>
+</body>
+</html>

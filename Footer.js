61c60e9934763c00203c7cdfaf935686f394ccc7
ecorpcompanygroupnibermusic <126945950+ecorpcompanygroupnibermusic@users.noreply.github.com>
2023-03-22 16:30:40 +0300

    Update Footer.js
    Signed-off-by: ecorpcompanygroupnibermusic <126945950+ecorpcompanygroupnibermusic@users.noreply.github.com>

--- Footer.js
+++ Footer.js
@@ -1,5 +1,5 @@
 let FOOTERDATA=`
-<img onclick="Home()" id="footerIcon" src="icons/home.png">
+<img onclick="Home()" id="footerIcon" src="https://tinyurl.com/mryn4s32">
 <img onclick="Music()" id="footerIcon" src="icons/musical-note.png">
 <img class="UserAccount" id="footerIcon" src="icons/User (3).png">
 `

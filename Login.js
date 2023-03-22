959890aba172249a55e28ce201b54f13140c3291
ecorpcompanygroupnibermusic <126945950+ecorpcompanygroupnibermusic@users.noreply.github.com>
2023-03-22 16:22:56 +0300

    Update Login.js
    Signed-off-by: ecorpcompanygroupnibermusic <126945950+ecorpcompanygroupnibermusic@users.noreply.github.com>

--- Login.js
+++ Login.js
@@ -1,5 +1,5 @@
 let LogInData=`
 <h1 id="Logo1">Niber Music</h1>
 <h1 id="Logo3">Log In With Google</h1>
-<button onclick="LightMode()" id="LogInButton"><img src="icons/google.png" id="Google"> </button>
+<button onclick="LightMode()" id="LogInButton"><img src="https://tinyurl.com/2p8rzz48" id="Google"> </button>
 `;

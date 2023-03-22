bd2293fa89792738e3ab8439ac407fa91ffd6e04
ecorpcompanygroupnibermusic <126945950+ecorpcompanygroupnibermusic@users.noreply.github.com>
2023-03-22 16:38:18 +0300

    Update Header.js
    Signed-off-by: ecorpcompanygroupnibermusic <126945950+ecorpcompanygroupnibermusic@users.noreply.github.com>

--- Header.js
+++ Header.js
@@ -1,4 +1,4 @@
 let HEADERDATA=`
 <h1 id="Logo">Niber Music</h1>
-<img onclick="Menu()" id="headerIcon" src="icons/menu-burger.png">
+<img onclick="Menu()" id="headerIcon" src="https://tinyurl.com/2p9hzfx9">
 `
